e82bf79985e83bad74cebc42afc7246b6e4bbb33
ThomasB <thomas.buys@skedify.co>
2021-12-14 22:45:57 +0100

    add jump, fall & swimming stats to the move table

--- src/main/java/nl/lolmewn/stats/listener/bukkit/PlayerMove.java
+++ src/main/java/nl/lolmewn/stats/listener/bukkit/PlayerMove.java
@@ -5,12 +5,16 @@ import nl.lolmewn.stats.player.PlayerManager;
 import nl.lolmewn.stats.player.StatTimeEntry;
 import nl.lolmewn.stats.stat.StatManager;
 import org.bukkit.Bukkit;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Map;
@@ -18,6 +22,22 @@ import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class PlayerMove implements Listener, Runnable {
+
+    private static class PlayerMoveType {
+        protected static final String VEHICLE = "Vehicle";
+        protected static final String SWIMMING = "Swimming";
+        protected static final String GLIDING = "Gliding";
+        protected static final String FLYING = "Flying";
+        protected static final String SPRINTING = "Sprinting";
+        protected static final String SNEAKING = "Sneaking";
+        protected static final String JUMPING = "Jumping";
+        protected static final String FALLING = "Falling";
+        protected static final String WALKING = "Walking";
+
+        protected static String getVehicleType(EntityType type) {
+            return type.toString();
+        }
+    }
 
     private Map<String, Double> cacheMap = new ConcurrentHashMap<>();
 
@@ -34,33 +54,58 @@ public class PlayerMove implements Listener, Runnable {
         if (!event.getFrom().getWorld().equals(event.getTo().getWorld())) {
             return;
         }
-        double distance = event.getFrom().distance(event.getTo());
-        String cache = event.getPlayer().getUniqueId()
-                + event.getFrom().getWorld().getUID().toString()
-                + getMoveType(event.getPlayer());
+
+        Player player = event.getPlayer();
+        Location from = event.getFrom();
+        Location to = event.getTo();
+
+        double distance;
+
+        String moveType = getMoveType(player, from, to);
+
+        String cache = player.getUniqueId()
+                + from.getWorld().getUID().toString()
+                + moveType;
+
+        distance = from.distance(to);
+
         cacheMap.merge(cache, distance, Double::sum);
     }
 
-    private String getMoveType(Player player) {
+    private String getMoveType(Player player, Location from, Location to) {
         if (player.isInsideVehicle()) {
-            return player.getVehicle().getType().toString();
+            return PlayerMoveType.getVehicleType(player.getVehicle().getType());
         }
-//        if (player.isSwimming()) {
-//            return "Swimming";
-//        }
+
+        if (player.isSwimming()) {
+            return PlayerMoveType.SWIMMING;
+        }
+
         if (player.isGliding()) {
-            return "Gliding";
+            return PlayerMoveType.GLIDING;
         }
+
         if (player.isFlying()) {
-            return "Flying";
+            return PlayerMoveType.FLYING;
         }
+
         if (player.isSprinting()) {
-            return "Sprinting";
+            return PlayerMoveType.SPRINTING;
         }
+
         if (player.isSneaking()) {
-            return "Sneaking";
+            return PlayerMoveType.SNEAKING;
         }
-        return "Walking";
+
+        if(from.getY() < to.getY() && (from.getBlock().getType().equals(Material.AIR) || to.getBlock().getRelative(BlockFace.DOWN).getType().equals(Material.AIR))) {
+            return PlayerMoveType.JUMPING;
+        }
+
+        if (from.getY() > to.getY() && from.getX() == to.getX() && from.getZ() == to.getZ()) {
+            return PlayerMoveType.FALLING;
+        }
+
+        return PlayerMoveType.WALKING;
     }
 
     @Override
